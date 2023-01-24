08992b5c5666f7fb433f86dc01452b89041b3149
theoneduke <104139400+theoneduke@users.noreply.github.com>
2023-01-24 09:54:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,13 @@ const giveaway = document.querySelector(`.giveaway`);
 const deadline = document.querySelector(`.deadline`);
 const items = document.querySelectorAll(`.deadline-format h4`);
 
-let futureDate = new Date(2023, 0, 25, 11, 30, 0);
+let tempDate = new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay = tempDate.getDate();
+
+// let futureDate = new Date(2023, 0, 25, 11, 30, 0);
+const futureDate = new Date (tempYear, tempMonth, tempDay + 1, 11, 30, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
